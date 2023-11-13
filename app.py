c001853ea3584c7d4a1e0fdc5fe052b781ee888c
ahmedsamir45 <ahmedsamer6788@gmail.com>
2023-11-13 22:40:07 +0200

    upload files

---
+++ app.py
@@ -0,0 +1,20 @@
+from webapp import app
+
+
+
+
+
+
+
+
+
+
+    
+
+if __name__ == "__main__" :
+    app.run(debug=True,port=8000)
+
+    #flaskapp\Scripts\activate
+    # pip install Flask-SQLAlchemy==2.5.1
+    # with app.app_context():
+    #     db.create_all()
