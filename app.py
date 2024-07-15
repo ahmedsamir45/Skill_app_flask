c6b383bf8778a61c860bbc0285854720de7f48ef
ahmedsamir45 <ahmedsamer6788@gmail.com>
2024-07-16 00:16:28 +0300

    host

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ app = create_app()
     
 
 if __name__ == "__main__" :
-    app.run(debug=True,port=8000)
+    app.run(debug=False,port=5000,host="0.0.0.0")
+
 
     #flaskapp\Scripts\activate
     # pip install Flask-SQLAlchemy==2.5.1
